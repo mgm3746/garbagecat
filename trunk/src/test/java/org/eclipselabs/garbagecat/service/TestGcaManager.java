c57c891f544c0ba4b5cd66a16ef3f44a75e67509
mgm101@gmail.com <mgm101@gmail.com@def1ecba-d854-2b5e-9c91-bce07a6d6549>
2011-05-19 01:21:57 +0000

    Issue #17: Test for NullPointerException during preparsing.

---
+++ trunk/src/test/java/org/eclipselabs/garbagecat/service/TestGcaManager.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * Garbage Cat                                                                *
+ *                                                                            *
+ * Copyright (c) 2008-2010 Red Hat, Inc.                                      *
+ * All rights reserved. This program and the accompanying materials           *
+ * are made available under the terms of the Eclipse Public License v1.0      *
+ * which accompanies this distribution, and is available at                   *
+ * http://www.eclipse.org/legal/epl-v10.html                                  *
+ *                                                                            *
+ * Contributors:                                                              *
+ *    Red Hat, Inc. - initial API and implementation                          *
+ ******************************************************************************/
+package org.eclipselabs.garbagecat.service;
+
+import java.io.File;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
+ * 
+ */
+public class TestGcaManager extends TestCase {
+
+	/**
+	 * Test for NullPointerException caused by Issue 17:
+	 * http://code.google.com/a/eclipselabs.org/p/garbagecat/issues/detail?id=17
+	 */
+	public void testNullPointerExceptionNotRaised() {
+		// TODO: Create File in platform independent way.
+		File testFile = new File("src/test/data/dataset31.txt");		
+		GcaManager jvmManager = new GcaManager();
+		try {
+			jvmManager.preprocess(testFile, null);
+		} catch (NullPointerException e) {
+			Assert.fail("Preprocessing results in NullPointerException.");
+		}
+	}
+}
