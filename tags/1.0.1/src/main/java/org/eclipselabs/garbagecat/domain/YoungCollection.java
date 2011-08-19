d1c59fd06aaa20dfa868915163ea3d0007ec1026
mgm101@gmail.com <mgm101@gmail.com@def1ecba-d854-2b5e-9c91-bce07a6d6549>
2011-08-19 13:05:45 +0000

    Tag 1.0.1 release.

---
+++ tags/1.0.1/src/main/java/org/eclipselabs/garbagecat/domain/YoungCollection.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ * Garbage Cat                                                                *
+ *                                                                            *
+ * Copyright (c) 2008-2010 Red Hat, Inc.                                      *
+ * All rights reserved. This program and the accompanying materials           *
+ * are made available under the terms of the Eclipse Public License v1.0      *
+ * which accompanies this distribution, and is available at                   *
+ * http://www.eclipse.org/legal/epl-v10.html                                  *
+ *                                                                            *
+ * Contributors:                                                              *
+ *    Red Hat, Inc. - initial API and implementation                          *
+ ******************************************************************************/
+package org.eclipselabs.garbagecat.domain;
+
+/**
+ * Young generation collection.
+ * 
+ * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
+ * 
+ */
+public interface YoungCollection {
+
+}
---
+++ tags/1.0.1/src/main/java/org/eclipselabs/garbagecat/preprocess/jdk/DateStampPreprocessAction.java
@@ -0,0 +1,107 @@
+/******************************************************************************
+ * Garbage Cat                                                                *
+ *                                                                            *
+ * Copyright (c) 2008-2010 Red Hat, Inc.                                      *
+ * All rights reserved. This program and the accompanying materials           *
+ * are made available under the terms of the Eclipse Public License v1.0      *
+ * which accompanies this distribution, and is available at                   *
+ * http://www.eclipse.org/legal/epl-v10.html                                  *
+ *                                                                            *
+ * Contributors:                                                              *
+ *    Red Hat, Inc. - initial API and implementation                          *
+ ******************************************************************************/
+package org.eclipselabs.garbagecat.preprocess.jdk;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipselabs.garbagecat.util.GcUtil;
+import org.eclipselabs.garbagecat.util.jdk.JdkMath;
+import org.eclipselabs.garbagecat.util.jdk.JdkRegEx;
+import org.eclipselabs.garbagecat.util.jdk.JdkUtil;
+
+/**
+ * <p>
+ * DATE_STAMP
+ * </p>
+ * 
+ * <p>
+ * Logging with a datestamp instead of the timestamp indicating the number of seconds after JVM startup. Enabled with
+ * the <code>-XX:+PrintGCDateStamps</code> option added in JDK 1.6 update 4.
+ * </p>
+ * 
+ * <p>
+ * It appears that initial implementations replace the timestamp with a datestamp and later versions of the JDK prefix
+ * the normal timestamp with a datestamp.
+ * </p>
+ * 
+ * <h3>Example Logging</h3>
+ * 
+ * <pre>
+ * 2010-02-26T09:32:12.486-0600: [GC [ParNew: 150784K-&gt;3817K(169600K), 0.0328800 secs] 150784K-&gt;3817K(1029760K), 0.0329790 secs] [Times: user=0.00 sys=0.00, real=0.03 secs]
+ * </pre>
+ * 
+ * Preprocessed:
+ * 
+ * <pre>
+ * 142.973: [GC [ParNew: 150784K-&gt;3817K(169600K), 0.0328800 secs] 150784K-&gt;3817K(1029760K), 0.0329790 secs] [Times: user=0.00 sys=0.00, real=0.03 secs]
+ * </pre>
+ * 
+ * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
+ * 
+ */
+public class DateStampPreprocessAction implements PreprocessAction {
+
+    /**
+     * Regular expressions defining the logging line.
+     */
+    private static final String REGEX_LINE = "^" + JdkRegEx.DATESTAMP + ": (.*)$";
+
+    /**
+     * The log entry for the event. Can be used for debugging purposes.
+     */
+    private String logEntry;
+
+    /**
+     * Create event from log entry.
+     * 
+     * @param logEntry
+     *            The log entry.
+     * @param jvmStartDate
+     *            The date and time the JVM was started.
+     */
+    public DateStampPreprocessAction(String logEntry, Date jvmStartDate) {
+        Pattern pattern = Pattern.compile(REGEX_LINE);
+        Matcher matcher = pattern.matcher(logEntry);
+        if (matcher.find()) {
+            String logEntryMinusDateStamp = matcher.group(11);
+            StringBuffer sb = new StringBuffer();
+            Date datestamp = GcUtil.parseDateStamp(matcher.group(1));
+            long diff = GcUtil.dateDiff(jvmStartDate, datestamp);
+            sb.append((JdkMath.convertMillisToSecs(diff)).toString());
+            sb.append(": ");
+            sb.append(logEntryMinusDateStamp);
+            this.logEntry = sb.toString();
+        }
+    }
+
+    public String getLogEntry() {
+        return logEntry;
+    }
+
+    public String getName() {
+        return JdkUtil.PreprocessActionType.DATE_STAMP.toString();
+    }
+
+    /**
+     * Determine if the logLine matches the logging pattern(s) for this event.
+     * 
+     * @param logLine
+     *            The log line to test.
+     * @return true if the log line matches the event pattern, false otherwise.
+     */
+    public static final boolean match(String logLine) {
+        return logLine.matches(REGEX_LINE);
+    }
+}
