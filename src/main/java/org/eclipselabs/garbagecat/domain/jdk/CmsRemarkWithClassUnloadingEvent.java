87493ab3acdc3fa8ca02f325133aa70e07a25991
mgm101 <mgm101@gmail.com>
2013-12-23 21:59:27 +0000

    Issue #38: JDK7 CMS_REMARK_WITH_CLASS_UNLOADING.

--- src/main/java/org/eclipselabs/garbagecat/domain/jdk/CmsRemarkWithClassUnloadingEvent.java
+++ src/main/java/org/eclipselabs/garbagecat/domain/jdk/CmsRemarkWithClassUnloadingEvent.java
@@ -33,8 +33,20 @@ import org.eclipselabs.garbagecat.util.jdk.JdkUtil;
  * <h3>Example Logging</h3>
  * </p>
  * 
+ * <p>
+ * 1) JDK 1.6:
+ * </p>
+ * 
  * <pre>
  * 76694.727: [GC[YG occupancy: 80143 K (153344 K)]76694.727: [Rescan (parallel) , 0.0574180 secs]76694.785: [weak refs processing, 0.0170540 secs]76694.802: [class unloading, 0.0363010 secs]76694.838: [scrub symbol &amp; string tables, 0.0276600 secs] [1 CMS-remark: 443542K(4023936K)] 523686K(4177280K), 0.1446880 secs]
+ * </pre>
+ * 
+ * <p>
+ * 2) JDK 1.7:
+ * </p>
+ * 
+ * <pre>
+ * 75.500: [GC[YG occupancy: 163958 K (306688 K)]75.500: [Rescan (parallel) , 0.0491823 secs]75.549: [weak refs processing, 0.0088472 secs]75.558: [class unloading, 0.0049468 secs]75.563: [scrub symbol table, 0.0034342 secs]75.566: [scrub string table, 0.0005542 secs] [1 CMS-remark: 378031K(707840K)] 541989K(1014528K), 0.0687411 secs] [Times: user=0.13 sys=0.00, real=0.07 secs]
  * </pre>
  * 
  * @author <a href="mailto:mmillson@redhat.com">Mike Millson</a>
@@ -63,10 +75,12 @@ public class CmsRemarkWithClassUnloadingEvent implements BlockingEvent {
     private static final String REGEX = "^" + JdkRegEx.TIMESTAMP + ": \\[GC\\[YG occupancy: " + JdkRegEx.SIZE + " \\("
             + JdkRegEx.SIZE + "\\)\\]" + JdkRegEx.TIMESTAMP + ": \\[Rescan \\(parallel\\) , " + JdkRegEx.DURATION
             + "\\]" + JdkRegEx.TIMESTAMP + ": \\[weak refs processing, " + JdkRegEx.DURATION + "\\]"
-            + JdkRegEx.TIMESTAMP + ": \\[class unloading, " + JdkRegEx.DURATION + "\\]" + JdkRegEx.TIMESTAMP
-            + ": \\[scrub symbol & string tables, " + JdkRegEx.DURATION + "\\] \\[1 CMS-remark: " + JdkRegEx.SIZE
-            + "\\(" + JdkRegEx.SIZE + "\\)\\] " + JdkRegEx.SIZE + "\\(" + JdkRegEx.SIZE + "\\), " + JdkRegEx.DURATION
-            + "\\]" + JdkRegEx.TIMES_BLOCK + "?[ ]*$";
+            + JdkRegEx.TIMESTAMP + ": \\[class unloading, " + JdkRegEx.DURATION + "\\](" + JdkRegEx.TIMESTAMP
+            + ": \\[scrub symbol & string tables, " + JdkRegEx.DURATION + "\\]|" + JdkRegEx.TIMESTAMP
+            + ": \\[scrub symbol table, " + JdkRegEx.DURATION + "\\]" + JdkRegEx.TIMESTAMP
+            + ": \\[scrub string table, " + JdkRegEx.DURATION + "\\]) \\[1 CMS-remark: " + JdkRegEx.SIZE + "\\("
+            + JdkRegEx.SIZE + "\\)\\] " + JdkRegEx.SIZE + "\\(" + JdkRegEx.SIZE + "\\), " + JdkRegEx.DURATION + "\\]"
+            + JdkRegEx.TIMES_BLOCK + "?[ ]*$";
     private static Pattern pattern = Pattern.compile(CmsRemarkWithClassUnloadingEvent.REGEX);
 
     /**
@@ -80,7 +94,7 @@ public class CmsRemarkWithClassUnloadingEvent implements BlockingEvent {
         if (matcher.find()) {
             timestamp = JdkMath.convertSecsToMillis(matcher.group(1)).longValue();
             // The last duration is the total duration for the phase.
-            duration = JdkMath.convertSecsToMillis(matcher.group(16)).intValue();
+            duration = JdkMath.convertSecsToMillis(matcher.group(21)).intValue();
         }
     }
 
