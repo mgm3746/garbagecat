85e248429f12fa446f5a5fd9d9aed54adbdb6b8c
mmillson <mmillson@localhost>
2020-09-09 10:36:17 -0400

    Fix header typos.

--- src/test/java/org/eclipselabs/garbagecat/preprocess/jdk/unified/TestUnifiedPreprocessAction.java
+++ src/test/java/org/eclipselabs/garbagecat/preprocess/jdk/unified/TestUnifiedPreprocessAction.java
@@ -1,14 +1,14 @@
 /**********************************************************************************************************************
  * garbagecat                                                                                                         *
  *                                                                                                                    *
- * Copyright (c) 2008-2020 Mike Millson                                                                              *
+ * Copyright (c) 2008-2020 Mike Millson                                                                               *
  *                                                                                                                    * 
  * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse *
  * Public License v1.0 which accompanies this distribution, and is available at                                       *
  * http://www.eclipse.org/legal/epl-v10.html.                                                                         *
  *                                                                                                                    *
  * Contributors:                                                                                                      *
- *    Mike Millson - initial API and implementation                                                                  *
+ *    Mike Millson - initial API and implementation                                                                   *
  *********************************************************************************************************************/
 package org.eclipselabs.garbagecat.preprocess.jdk.unified;
 
