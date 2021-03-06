b1fdeabc99a94fccc6275b54aa4166b4217139ac
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2021-03-06 09:10:45 +0100

    tests added

--- src/test/java/org/eclipselabs/garbagecat/util/MemoryTest.java
+++ src/test/java/org/eclipselabs/garbagecat/util/MemoryTest.java
@@ -1,5 +1,6 @@
 package org.eclipselabs.garbagecat.util;
 
+import static org.eclipselabs.garbagecat.util.Memory.bytes;
 import static org.eclipselabs.garbagecat.util.Memory.kilobytes;
 import static org.eclipselabs.garbagecat.util.Memory.megabytes;
 import static org.eclipselabs.garbagecat.util.Memory.memory;
@@ -17,11 +18,24 @@ public class MemoryTest {
 	public void canParse() {
 		Memory oneKilobyte = memory("1K");
 		assertThat(oneKilobyte.getValue(KILOBYTES), is(equalTo(1L)));
-		assertThat(oneKilobyte.getValue(BYTES), equalTo(1024L));
+		assertThat(oneKilobyte.getValue(BYTES), is(equalTo(1024L)));
 	}
 
 	@Test
-	public void canCompareTwoInstances() {
+	public void parseIsCaseInsensitiv() {
+		String string = "8K";
+		assertThat(memory(string.toLowerCase()), is(equalTo(memory(string.toUpperCase()))));
+	}
+
+	@Test
+	public void hasToString() {
+		Memory thirtyTwoMegabytes = memory("32M");
+		assertThat(thirtyTwoMegabytes.toString(), is(equalTo("32M")));
+		assertThat(thirtyTwoMegabytes.convertTo(KILOBYTES).toString(), is(equalTo(32 * 1024 + "K")));
+	}
+
+	@Test
+	public void canEqualOnInstancesUsingDifferentUnits() {
 		assertThat(kilobytes(2048), is(equalTo(megabytes(2))));
 	}
 
@@ -33,6 +47,17 @@ public class MemoryTest {
 	@Test
 	public void canSubtract() {
 		assertThat(megabytes(2).minus(kilobytes(1)), is(equalTo(kilobytes(2047))));
+	}
+
+	@Test
+	public void canCompare() {
+		assertThat(bytes(1).greaterThan(bytes(2)), is(false));
+		assertThat(bytes(1).greaterThan(bytes(1)), is(false));
+		assertThat(bytes(2).greaterThan(bytes(1)), is(true));
+
+		assertThat(bytes(1).lessThan(bytes(2)), is(true));
+		assertThat(bytes(1).lessThan(bytes(1)), is(false));
+		assertThat(bytes(2).lessThan(bytes(1)), is(false));
 	}
 
 }
